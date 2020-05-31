e3ddb6b13cf71369dbd3743ed556f07b664c73b4
NaabZer <anton.gefvert@gmail.com>
2020-05-31 15:03:08 +0200

    Add enum to component_type

--- material-resume-backend/resume_components/models.py
+++ material-resume-backend/resume_components/models.py
@@ -1,16 +1,26 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 
 class Component(models.Model):
-    component_type = models.CharField(max_length=32)
+    class ComponentTypes(models.TextChoices):
+        CARD = 'C_CARD', _('Card Component')
+    component_type = models.CharField(
+        max_length=32,
+        choices=ComponentTypes.choices
+    )
     col = models.IntegerField(default=0)
     row = models.IntegerField(default=0)
     width = models.IntegerField(default=0)
     height = models.IntegerField(default=0)
     inside_grid = models.ForeignKey('self', on_delete=models.CASCADE,
                                     blank=True, null=True)
+
+    def __str__(self):
+        return self.component_type + "(" +\
+                [self.row, self.col, self.width, self.height].join(',') + ")"
 
 
 class SettingsRow(models.Model):
