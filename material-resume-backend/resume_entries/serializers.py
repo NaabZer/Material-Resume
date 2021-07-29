f233f379eec540653b11a33c3b9127eea67845e6
NaabZer <anton.gefvert@gmail.com>
2021-07-29 12:41:42 +0200

    Make creating nested text entries possible on back-end

--- material-resume-backend/resume_entries/serializers.py
+++ material-resume-backend/resume_entries/serializers.py
@@ -18,7 +18,6 @@ class ExperienceSerializer(serializers.HyperlinkedModelSerializer):
     def create(self, validated_data):
         entries_data = validated_data.pop('entries')
         experience = Experience.objects.create(**validated_data)
-        print(entries_data)
 
         entries_list = []
         for entry_data in entries_data:
@@ -40,24 +39,24 @@ class TextEntrySerializer(serializers.HyperlinkedModelSerializer):
 
 
 class TextSerializer(serializers.HyperlinkedModelSerializer):
-    entries = TextEntrySerializer(many=True, read_only=True)
+    entries = TextEntrySerializer(many=True)
 
     class Meta:
         model = Text
         fields = ['url', 'id', 'entries']
 
     def create(self, validated_data):
-        text = Text.objects.create(**validated_data)
+        entries_data = validated_data.pop('entries')
+        text_obj = Text.objects.create(**validated_data)
 
-        owner_langs = validated_data['owner'].languages.all()
         entries_list = []
-        for language in owner_langs:
+        for entry_data in entries_data:
             entry = TextEntry.objects.create(
-                    lang=language.language,
-                    text_obj=text
+                    text_obj=text_obj,
+                    **entry_data
                     )
             entries_list.append(entry)
-        text.entries.set(entries_list)
-        text.save()
+        text_obj.entries.set(entries_list)
+        text_obj.save()
 
-        return text
+        return text_obj
