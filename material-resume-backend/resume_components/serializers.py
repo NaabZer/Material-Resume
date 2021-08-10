a82c85e1f6d6a8f800f77969f67f8d7948036f35
NaabZer <anton.gefvert@gmail.com>
2021-08-10 17:05:58 +0200

    Make serializers handle existing, nested pages/components

--- material-resume-backend/resume_components/serializers.py
+++ material-resume-backend/resume_components/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Resume, Page, Component, SettingsRow
+from rest_framework_recursive.fields import RecursiveField
 
 
 class SettingsRowSerializer(serializers.HyperlinkedModelSerializer):
@@ -7,12 +8,7 @@ class SettingsRowSerializer(serializers.HyperlinkedModelSerializer):
         model = SettingsRow
         fields = ['id', 'url', 'setting', 'value', 'resume',
                   'component', 'page']
-
-
-class RecursiveField(serializers.Serializer):
-    def to_representation(self, value):
-        serializer = self.parent.parent.__class__(value, context=self.context)
-        return serializer.data
+        extra_kwargs = {'id': {'read_only': False}}
 
 
 class ComponentSerializer(serializers.HyperlinkedModelSerializer):
@@ -24,6 +20,27 @@ class ComponentSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['id', 'url', 'inside_page', 'inside_component',
                   'component_type', 'row', 'col', 'height', 'width',
                   'settings', 'child_components']
+        extra_kwargs = {'id': {'read_only': False}}
+
+    def update(self, instance, validated_data):
+        children_data = validated_data.pop('child_components')
+        settings_data = validated_data.pop('settings')
+
+        for child_data in children_data:
+            print('----')
+            print(child_data)
+            child = Component.objects.get(pk=child_data.get('id'))
+            print(child)
+            ComponentSerializer().update(child, child_data)
+
+        """
+        for setting_data in settings_data:
+            setting = SettingsRow.objects.get(pk=setting_data.get('id'))
+            SettingsRowSerializer().update(setting, setting_data)
+        """
+
+        return super(ComponentSerializer, self).update(instance,
+                                                       validated_data)
 
 
 class PageSerializer(serializers.HyperlinkedModelSerializer):
@@ -34,6 +51,27 @@ class PageSerializer(serializers.HyperlinkedModelSerializer):
         model = Page
         fields = ['id', 'url', 'resume', 'page_num', 'settings',
                   'child_components']
+        extra_kwargs = {'id': {'read_only': False}}
+
+    def update(self, instance, validated_data):
+        children_data = validated_data.pop('child_components')
+        settings_data = validated_data.pop('settings')
+
+        for child_data in children_data:
+            print('####')
+            print(child_data)
+            child = Component.objects.get(pk=child_data.get('id'))
+            print(child)
+            ComponentSerializer().update(child, child_data)
+
+        """
+        for setting_data in settings_data:
+            setting = SettingsRow.objects.get(pk=setting_data.get('id'))
+            SettingsRowSerializer().update(setting, setting_data)
+        """
+
+        return super(PageSerializer, self).update(instance,
+                                                  validated_data)
 
 
 class ResumeSerializer(serializers.HyperlinkedModelSerializer):
@@ -43,6 +81,24 @@ class ResumeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Resume
         fields = ['id', 'url', 'name', 'pages', 'settings']
+
+    def update(self, instance, validated_data):
+        print(validated_data)
+        pages_data = validated_data.pop('pages')
+        settings_data = validated_data.pop('settings')
+
+        for page_data in pages_data:
+            page = Page.objects.get(pk=page_data.get('id'))
+            PageSerializer().update(page, page_data)
+
+        """
+        for setting_data in settings_data:
+            setting = SettingsRow.objects.get(pk=setting_data.get('id'))
+            SettingsRowSerializer().update(setting, setting_data)
+        """
+
+        return super(ResumeSerializer, self).update(instance,
+                                                    validated_data)
 
 
 class ResumeListSerializer(serializers.HyperlinkedModelSerializer):
