24e94dff3856f25f270d3bfe8b4d79d8b14eff03
NaabZer <anton.gefvert@gmail.com>
2021-09-07 14:49:41 +0200

    Remake microservice to take entries/components and render a special page to pdf

--- material-resume-pdf-generator/pdf.js
+++ material-resume-pdf-generator/pdf.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json({limit: '50mb'}));
 const port = 3001
 
 app.post('/pdf', (req, res) => {
-  printPDF(req.body.csv).then(pdf => {
+  printPDF(req.body.components, req.body.entries).then(pdf => {
     res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length })
     res.send(pdf)
   })
@@ -18,13 +18,24 @@ app.post('/pdf', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
  
-async function printPDF(path) {
+async function printPDF(components, entries) {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto(path, {waitUntil: 'networkidle2'});
-  const pdf = await page.pdf({ format: 'A4', printBackground: true, omitBackground: true});
+  page.setDefaultNavigationTimeout(10000);
+  await page.goto('http://localhost:3000/resumes/exportpdf', {waitUntil: 'networkidle0'});
+  const ret = await page.evaluate((components, entries) => { 
+    return window.setPdfData(components, entries); 
+  }, components, entries);
+  await page.emulateMediaType('screen')
+  const content = await page.content();
+  await page.evaluate(() => { window.scrollBy(0, window.innerHeight); })
 
+  const pdf = await page.pdf({ format: 'A4', printBackground: true, omitBackground: true});
 
   await browser.close();
   return pdf
