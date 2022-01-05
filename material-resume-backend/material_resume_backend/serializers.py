8a9a42e4336a01251206d4ec0a8cc5ed7febf7e1
NaabZer <anton.gefvert@gmail.com>
2022-01-05 15:14:06 +0100

    Make language change remove/add entries to user

--- material-resume-backend/material_resume_backend/serializers.py
+++ material-resume-backend/material_resume_backend/serializers.py
@@ -3,6 +3,7 @@ from django.core import exceptions
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from .models import User, Language
+from resume_entries.models import ExperienceEntry, TextEntry
 
 
 class LanguageSerializer(serializers.ModelSerializer):
@@ -33,9 +34,18 @@ class UserSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         languages_data = validated_data.pop('languages')
-        instance.languages.clear()
 
+        # Check for new languages, add empty entries for each new lang
         for lang_data in languages_data:
+            lang_str = lang_data.get('language')
+
+            lang_exists = instance.languages.filter(language=lang_str).exists()
+            if lang_exists:
+                print("{} exists".format(lang_str))
+                # Do nothing if user alreadu has language
+                continue
+
+            # Add language to user
             lang = Language.objects.filter(
                     language=lang_data.get('language')).first()
             if lang:
@@ -44,6 +54,37 @@ class UserSerializer(serializers.ModelSerializer):
                 new_lang = Language.objects.create(
                         language=lang_data.get('language'))
                 instance.languages.add(new_lang)
+
+            # Add empy experience entries for new language
+            for exp in instance.experiences.all():
+                entry = ExperienceEntry.objects.create(
+                        experience=exp,
+                        lang=lang
+                    )
+                print('exp added')
+                print(entry)
+                exp.entries.add(entry)
+
+            # Add empy text entries for new language
+            for text in instance.texts.all():
+                entry = TextEntry.objects.create(
+                        text_obj=text,
+                        lang=lang
+                    )
+                print('text added')
+                print(entry)
+                text.entries.add(entry)
+
+        # Check if old language has been deleted
+        to_remove = []
+        lang_data = [lang.get('language') for lang in languages_data]
+        for lang in instance.languages.all():
+            if lang.language not in lang_data:
+                print("Removing {}".format(lang.language))
+                to_remove.append(lang)
+
+        for remove_lang in to_remove:
+            instance.languages.remove(remove_lang)
 
         return super(UserSerializer, self).update(instance,
                                                   validated_data)
